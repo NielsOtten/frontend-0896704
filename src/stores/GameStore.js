d1858aa327ba989600e6dc462de327cd62f231a7
Niels <niels@burst-digital.com>
2017-06-21 10:57:41 +0200

    Remove unnecessary observerables from gameStore

--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -2,10 +2,10 @@ import { observable } from 'mobx';
 
 class GameStore {
   @observable playing = false;
-  @observable grid = [];
-  @observable hits = [];
   @observable targets = [];
   @observable goodTargets = 0;
+  hits = [];
+  grid = [];
 
   getGoodTargets() {
     this.goodTargets = this.hits.filter(hit => this.targets.includes(hit.index)).length;
