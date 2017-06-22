d45d0b7bfa77b489accfee25a11aa688bab226b0
Niels <niels@burst-digital.com>
2017-06-22 21:58:51 +0200

    Add background audio

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,21 +1,31 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import styles from './styles.scss';
+import submarine from '../../sound/submarine.mp3';
 
-function App({ children }) {
-  return (
-    <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/settings'>Settings</Link>
-      </nav>
-      <div className={styles.content}>
-        {children}
+class App extends Component {
+  componentDidMount() {
+    this.audio.volume -= 0.6;
+  }
+
+  render() {
+    return (
+      <div>
+        <audio ref={a => this.audio = a} autoPlay loop>
+          <source src={submarine} />
+        </audio>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/settings'>Settings</Link>
+        </nav>
+        <div className={styles.content}>
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 App.propTypes = {
