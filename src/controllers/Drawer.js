b9ddc626fc26e0d7fc5252ff39fcb59495b9771b
Niels <niels@burst-digital.com>
2017-06-23 11:02:44 +0200

    Add comments to Drawer.js

--- src/controllers/Drawer.js
+++ src/controllers/Drawer.js
@@ -1,4 +1,6 @@
-
+/**
+ * This Drawer class is used to organise the drawings on a canvas.
+ */
 class Drawer {
   constructor(canvasElement) {
     this.canvas = canvasElement;
@@ -6,14 +8,25 @@ class Drawer {
     this.drawArray = [];
   }
 
+  /**
+   * Add a function to the drawArray.
+   *
+   * @param func
+   */
   addFunction(func) {
     this.drawArray.push(func);
   }
 
+  /**
+   * Remove all functions from the drawArray.
+   */
   resetDrawer() {
     this.drawArray = [];
   }
 
+  /**
+   * Draw everything that is inside the drawArray.
+   */
   draw() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.drawArray.forEach((func) => { func(this); });
