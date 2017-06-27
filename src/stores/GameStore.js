013eeb739f1f2629346b14a3a4ac523aafd27a3b
Niels <niels@burst-digital.com>
2017-06-27 16:11:35 +0200

    Fix board rotation

--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -15,54 +15,55 @@ class GameStore {
       let index = 0;
       switch(hit.index) {
         case 0:
-          index = 12;
-          break;
-        case 1:
-          index = 13;
-          break;
-        case 2:
-          index = 14;
-          break;
-        case 3:
           index = 15;
           break;
+        case 1:
+          index = 14;
+          break;
+        case 2:
+          index = 13;
+          break;
+        case 3:
+          index = 12;
+          break;
         case 4:
-          index = 8;
-          break;
-        case 5:
-          index = 9;
-          break;
-        case 6:
-          index = 10;
-          break;
-        case 7:
           index = 11;
           break;
+        case 5:
+          index = 10;
+          break;
+        case 6:
+          index = 9;
+          break;
+        case 7:
+          index = 8;
+          break;
         case 8:
-          index = 4;
-          break;
-        case 9:
-          index = 5;
-          break;
-        case 10:
-          index = 6;
-          break;
-        case 11:
           index = 7;
           break;
+        case 9:
+          index = 6;
+          break;
+        case 10:
+          index = 5;
+          break;
+        case 11:
+          index = 4;
+          break;
         case 12:
-          index = 0;
-          break;
-        case 13:
-          index = 1;
-          break;
-        case 14:
-          index = 2;
-          break;
-        case 15:
           index = 3;
           break;
+        case 13:
+          index = 2;
+          break;
+        case 14:
+          index = 1;
+          break;
+        case 15:
+          index = 0;
+          break;
       }
+      console.log(this.targets[0], index, hit.index);
       return this.targets.includes(index);
     }).length;
   }
